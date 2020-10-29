8fb0f1360d205a95df91ff5f0d51e156e5d95b97
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2020-10-29 18:51:54 -0400

    added generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -5,9 +5,21 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
+}
+
+function generatePassword(){
+  //get desired length of password
+  var charLength = prompt("How long do you want your password to be? (8-128 characters)");
+
+  if(charLength >= 8 && charLength <= 128){
+
+  }
+  else{
+    generatePassword();
+  }
 }
 
 // Add event listener to generate button
