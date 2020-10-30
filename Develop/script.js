3b2c251aad49b250b85260a442cde0626982c686
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2020-10-29 22:37:20 -0400

    add checkChar function

--- Develop/script.js
+++ Develop/script.js
@@ -5,27 +5,42 @@ var generateBtn = document.querySelector("#generate");
 function writePassword(event) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
 
 }
 
-function generatePassword(){
+function generatePassword() {
 
 }
 
-function checkLength(){
-    //get desired length of password
-    var charLength = prompt("How long do you want your password to be? (8-128 characters)");
+function checkLength() {
+  //get desired length of password
+  var charLength = prompt("How long do you want your password to be? (8-128 characters)");
 
-    if(charLength >= 8 && charLength <= 128){
-      alert("Password length is good!");
-      return charLength;
-    }
-    else{
-      alert("Password length is not valid, password must be between 8 and 128 characters")
-      checkLength();
-    }
+  if (charLength >= 8 && charLength <= 128) {
+    alert("Password length is good!");
+    return charLength;
+  }
+  else {
+    alert("Password length is not valid, password must be between 8 and 128 characters")
+    checkLength();
+  }
+}
+
+function checkChar() {
+  var charTypes = [
+    "lowercase",
+    "uppercase",
+    "numeric",
+    "special"
+  ];
+
+  var isLowercase = true;
+  var isUppercase = true;
+  var isNumeric = true;
+  var isSpecial = true;
+
 }
 
 // Add event listener to generate button
